b7b9df2b6dad8beb84831deb9854610455296353
Pedro Reis <pedroreisv@gmail.com>
2017-02-16 16:28:05 -0200

    Add action types, auth_reducer and improved signin component

--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react'
-import { Field, reduxForm } from 'redux-form'
+import { reduxForm } from 'redux-form'
+import * as actions from '../../actions'
 
 class Signin extends Component {
   handleFormSubmit({ email, password }) {
-    console.log(email, password)
+    this.props.signinUser({ email, password })
+  }
+
+  renderAlert() {
+    if (this.props.errorMessage) {
+      return (
+        <div className="alert alert-danger">
+          <strong>Oops!</strong> {this.props.errorMessage}
+        </div>
+      )
+    }
   }
 
   render() {
@@ -13,19 +24,25 @@ class Signin extends Component {
       <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))} >
         <fieldset className="form-group">
           <label>Email:</label>
-          <Field name="email" component="input" {...email} className="form-control"/>
+          <input {...email} className="form-control"/>
         </fieldset>
         <fieldset className="form-group">
           <label>Password:</label>
-          <Field name="password" component="input" {...password} className="form-control"/>
+          <input {...password} type="password" className="form-control"/>
         </fieldset>
+        {this.renderAlert()}
         <button action="submit" className="btn btn-primary">Sign in</button>
      </form>
     )
   }
 }
 
-export default reduxForm({
-  form: 'signin',
-  fields: ['email', 'password']
-})(Signin)
+function mapStateToProps(state) {
+  return { errorMessage: state.auth.error }
+}
+
+export default reduxForm(
+  { form: 'signin', fields: ['email', 'password'] },
+  mapStateToProps,
+  actions
+)(Signin)
