2312a3b632f147aea97a9dd79ca514c2be4de560
Pedro Reis <pedroreisv@gmail.com>
2017-02-16 14:58:20 -0200

    Add Header component and Signin form component

---
+++ src/components/auth/signin.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import { Field, reduxForm } from 'redux-form'
+
+class Signin extends Component {
+  handleFormSubmit({ email, password }) {
+    console.log(email, password)
+  }
+
+  render() {
+    const { handleSubmit, fields: { email, password } } = this.props
+
+    return ( 
+      <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))} >
+        <fieldset className="form-group">
+          <label>Email:</label>
+          <Field name="email" component="input" {...email} className="form-control"/>
+        </fieldset>
+        <fieldset className="form-group">
+          <label>Password:</label>
+          <Field name="password" component="input" {...password} className="form-control"/>
+        </fieldset>
+        <button action="submit" className="btn btn-primary">Sign in</button>
+     </form>
+    )
+  }
+}
+
+export default reduxForm({
+  form: 'signin',
+  fields: ['email', 'password']
+})(Signin)
